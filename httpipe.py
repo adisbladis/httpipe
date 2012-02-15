f91d7cdf96f0b81c40ba9bfed9fe6f27c2a19a67
adisbladis <adis@blad.is>
2012-02-15 09:50:29 +0100

    Initial commit

---
+++ httpipe.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+'''
+Written by adisbladis <adis@blad.is>
+
+Licensed under the terms of GPLv3
+'''
+
+import http.server
+import sys
+import argparse
+
+class requestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        if args.f:
+            self.send_header('Content-Disposition', 'attachment; filename=%s' % (args.f))
+        self.send_header('Content-type', 'application/octet-stream')
+        self.end_headers()
+
+        sys.stdin = sys.stdin.detach()
+        while True:
+            s = sys.stdin.read(128)
+            if not s:
+                break
+            self.wfile.write(memoryview(s))
+
+parser = argparse.ArgumentParser(description='Httpipe - Reads data from stdin and serves over http')
+parser.add_argument('-p', action='store', required=False, type=int, default=8080, help='Port (default: 8080)')
+parser.add_argument('-a', action='store', required=False, type=str, default='0.0.0.0', help='Address (default: 0.0.0.0)')
+parser.add_argument('-f', action='store', required=False, type=str, default=False, help='Set filename header')
+args=parser.parse_args()
+
+httpd = http.server.HTTPServer((args.a,args.p),requestHandler)
+httpd.handle_request()
